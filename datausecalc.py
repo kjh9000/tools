6e3e78c4a72fdd5bb702962934e2c3dd0619515b
kevin <kjh9000@mail.com>
2021-05-31 07:13:57 -0700

    A data use calculator for Tello users

---
+++ datausecalc.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+# datausecalc.py -  A data usage calculator for Tello users
+# github.com/kjh9000
+
+''' Instructions - Copy and paste the wall of text rom the website and paste it into datausecalc.txt. Note the potential for fencepost errors. You are responsible for choosing what days to include in the calculation.
+
+Note there is a bug. The data needs to be a float. For example, 700 MB needs to be changed to 700.0 MB.
+'''
+
+import re
+
+try:
+    testtext = open('datausecalc.txt')
+except (NameError, FileNotFoundError):
+    print("This program requires a datausecalc.txt file. You can either create it or download it.")
+else:
+    sum = 0
+    count = 0
+    for i in re.compile(r'(\d{1,5}\.\d{1,2})').findall(testtext.read()):
+        count += 1
+        sum += float(i)
+    if sum == 0:
+        print("datausecalc.txt contains no useful data.")
+    else:
+        print("You have used about", round(sum, 2), "MB of data in", count, "days.")
+        print("You use an average of " + str(round(sum / count, 2)) + " MB per day.")

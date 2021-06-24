94cf1764c2592f3d7960270ee51ca0151d4f9fa3
kevin <kjh9000@mail.com>
2021-06-23 19:39:05 -0700

    A data use calculator for Tello users

--- datausecalc.py
+++ datausecalc.py
@@ -3,7 +3,7 @@
 # datausecalc.py -  A data usage calculator for Tello users
 # github.com/kjh9000
 
-''' Instructions - Copy and paste the wall of text rom the website and paste it into
+''' Instructions - Copy and paste the wall of text from the website and paste it into
 datausecalc.txt. Note the potential for fencepost errors. You are responsible for 
 choosing what days to include in the calculation.
 
